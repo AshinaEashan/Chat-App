ae9c45abd99ce7fc55be7acd9de83aed0eedf993
AshinaEashan <ashinaeashan@gmail.com>
2024-02-13 16:07:22 +0530

    Websocket config and event listener classes.

---
+++ src/main/java/com/ashina/chatapp/config/WebSocketEventListener.java
@@ -0,0 +1,18 @@
+package com.ashina.chatapp.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    public void handleWebSocketDisconnectListner(
+            SessionDisconnectEvent event
+    ){
+        //TODO -- to be implemented
+    }
+}
